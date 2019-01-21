4c821d3412371718ab5ecc45b5c877cc631d2ee1
jz1007 <jzavilla+code@alumni.nd.edu>
2019-01-20 20:04:15 -0500

    Update p009.py

--- p009.py
+++ p009.py
@@ -1,18 +1,11 @@
-#pythagorean triplet that equals 1000
+# find the triplet(s) that adds up to a number (indicated by variable 'n')
 
-import numpy as np
+def p_triplet(n):  
+    for a in range(1,n+1,1):
+        for b in range (a+1,n+1,1):
+            c = (a**2 + b**2)**(1/2)
+            if (a + b + c) == n:
+                print(a, b, c) #triplet
+                print('Product is: ' + str(int(a*b*c))) #this is the answer
 
-z = 250 ##use 'z' for 'c' in a**2 + b**2 = c**2
-
-def pythag_finder(c2): 
-    c = int(c2**(1/2))
-    pythag_tuples = []  #list of a's and b's that work with "c" variable
-    for a in range(1,c,1):
-        for b in range(a,c,1):
-            pythag = a**2 + b**2
-            if pythag == c2:
-                pythag_tuples.append([a,b,c])  #append to list
-            else:
-                continue
-    print(np.array(pythag_tuples))  #output a multi-dimentional array for easy reading / analysis later
-pythag_finder(z)
+p_triplet(1000)
